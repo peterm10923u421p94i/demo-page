9678f0fb8554746bd1299a72ffbd6953232d457c
peterm10923u421p94i <petermerk@hunschool.org>
2026-04-20 21:16:41 -0400

    Update stcript.js

--- stcript.js
+++ stcript.js
@@ -1,12 +1,7 @@
-// 1. Find the button in the HTML using its ID
+// This tells the browser to find the button
 const campfireButton = document.getElementById('fireButton');
 
-// 2. Tell the button to "listen" for a click
+// This tells the browser what to do when it's clicked
 campfireButton.addEventListener('click', function() {
-    
-    // 3. This is the action that happens when the click occurs
     alert("🔥 The campfire is crackling! Stay cozy, traveler.");
-    
-    // Bonus: Log a message to the developer console too
-    console.log("The user lit the fire!");
 });
